6a9845a364da881ff3f228ed35a7f91a5fb16be3
rojesh mote <rojesh123mote@gmail.com>
2021-02-19 19:23:20 +0545

    added database connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-dotenv.config();
-app.use(cors());
+require("dotenv").config();
 
 const PORT = process.env.PORT;
+
+app.use(cors());
+
+const URI = process.env.ATLAS_URI;
+const dbParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(URI, dbParams);
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
